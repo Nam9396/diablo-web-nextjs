dd1fde23a63a322d7264cf923338d07dd054dc08
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-17 20:28:22 +0700

    change backend URL

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://diablo-web-backend.onrender.com';
+export const BASE_URL = 'https://backend.bibohealth.com';
 export const DISCORD_URL = 'api/discord';
 export const USERS_URL = 'api/user';
 export const ORDER_URL = 'api/order';
