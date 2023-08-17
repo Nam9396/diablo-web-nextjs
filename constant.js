8805666918cd4c8c1cc2743ed7fcf8b9e281e46a
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-17 21:09:09 +0700

    test auth with localhost

--- constant.js
+++ constant.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'https://backend.bibohealth.com';
+// export const BASE_URL = 'https://backend.bibohealth.com';
+export const BASE_URL = 'http://localhost:10000';
 export const DISCORD_URL = 'api/discord';
 export const USERS_URL = 'api/user';
 export const ORDER_URL = 'api/order';
