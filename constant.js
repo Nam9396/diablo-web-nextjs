f2eafa91261e427ef96f591c8b5e66388b5e533a
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-17 11:27:54 +0700

    change BASE_URL

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://diablo-web-backend.onrender.com/';
 export const DISCORD_URL = 'api/discord';
 export const USERS_URL = 'api/user';
 export const ORDER_URL = 'api/order';
