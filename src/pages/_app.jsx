e4dfd0a618a37ee5d0979478bed6ae78d306af79
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-18 09:36:44 +0700

    add head for favicon

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 import { Footer } from '@/components/Footer'
-// import Header from '@/components/Header'
+import Head from 'next/head.js'
 import '@/styles/tailwind.css'
 import 'focus-visible'
 import { ToastContainer } from 'react-toastify';
@@ -35,10 +35,13 @@ export default function App({
       <Provider store={store}>
           <div className="fixed inset-0 flex justify-center bg-zinc-900"></div>
           <div className="relative flex w-full flex-col">
+            <Head>
+              <link rel="icon" href="/icon-1.ico" sizes="any" />
+            </Head>
             <Header />
-              <main className="flex-auto">
-                <Component previousPathname={previousPathname} {...pageProps} />
-              </main>
+            <main className="flex-auto">
+              <Component previousPathname={previousPathname} {...pageProps} />
+            </main>
             <Footer />
           </div>
           <ToastContainer autoClose={2000} hideProgressBar={true} />
