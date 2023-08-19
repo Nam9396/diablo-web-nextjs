7dfd18e1db34ecfd3ab1465082c224af76ac4c4f
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-19 17:11:54 +0700

    adjust prose

--- src/components/Prose.jsx
+++ src/components/Prose.jsx
@@ -2,6 +2,7 @@ import clsx from 'clsx'
 
 export function Prose({ children, className }) {
   return (
-    <div className={clsx(className, 'prose dark:prose-invert')}>{children}</div>
+    // <div className={clsx(className, 'prose prose-h2:text-zinc-100 prose-h3:text-zinc-100 prose-p:text-zinc-400')}>{children}</div>
+    <div className={clsx(className, 'prose')}>{children}</div>
   )
 }
