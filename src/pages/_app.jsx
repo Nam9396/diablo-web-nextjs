dff37e5e834139b2af944140316d04a4741ecafd
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-19 13:58:36 +0700

    add favicon new

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -35,9 +35,15 @@ export default function App({
       <Provider store={store}>
           <div className="fixed inset-0 flex justify-center bg-zinc-900"></div>
           <div className="relative flex w-full flex-col">
-            {/* <Head>
-              <link rel="icon" href="/icon-1.ico" sizes="any" />
-            </Head> */}
+            <Head>
+              <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+              <link rel="manifest" href="/site.webmanifest" />
+              <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+              <meta name="msapplication-TileColor" content="#da532c" />
+              <meta name="theme-color" content="#ffffff" />
+            </Head>
             <Header />
             <main className="flex-auto">
               <Component previousPathname={previousPathname} {...pageProps} />
