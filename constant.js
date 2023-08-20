aa36d2357bb7e2c75bc6f70a5f454675f83c4047
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-20 18:51:10 +0700

    change backend url

--- constant.js
+++ constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://backend.bibohealth.com';
+export const BASE_URL = 'https://backend.shadowgg.com';
 // export const BASE_URL = 'http://localhost:10000';
 export const DISCORD_URL = 'api/discord';
 export const USERS_URL = 'api/user';
