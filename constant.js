bed163226348724d05f66a418710cf988d1568a6
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-26 11:31:11 +0700

    no commit

--- constant.js
+++ constant.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = 'https://backend.shadowgg.com';
+export const BASE_URL = 'https://backend.shadowgg.com';
 // export const BASE_URL = 'http://localhost:10000';
-export const BASE_URL = 'https://diablo-backend.as.r.appspot.com';
+// export const BASE_URL = 'https://diablo-backend.as.r.appspot.com';
 export const DISCORD_URL = 'api/discord';
 export const USERS_URL = 'api/user';
 export const ORDER_URL = 'api/order';
